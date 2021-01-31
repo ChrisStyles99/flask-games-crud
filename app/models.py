7da0b9eaa0d6865929caa0e80014e5a00c7d32c4
ChrisStyles99 <chrisrdz99@hotmail.com>
2021-01-30 22:07:09 -0600

    Added Video Game database Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from .extensions import db
 
 class VideoGame(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  year = db.Column(db.Integer, nullable=False)
+  image_url = db.Column(db.String(1024), nullable=False)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from .models import VideoGame
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+  video_games = VideoGame.query.all()
+  print(video_games)
   return 'Home'
